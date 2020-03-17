bf0690eeb29c8c17d2869649b1058e40c93bdf8e
stefan <dancewithkeyboard@qq.com>
2020-03-17 15:48:50 +0800

    migrate to androidx

--- app/src/main/java/me/stefan/easybehavior/demo1/behavior/AppBarLayoutOverScrollViewBehavior.java
+++ app/src/main/java/me/stefan/easybehavior/demo1/behavior/AppBarLayoutOverScrollViewBehavior.java
@@ -3,10 +3,13 @@ package me.stefan.easybehavior.demo1.behavior;
 import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
+
 import com.google.android.material.appbar.AppBarLayout;
+
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.view.ViewCompat;
 import androidx.appcompat.widget.Toolbar;
+
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,6 +42,8 @@ public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
     private boolean isRecovering = false;//是否正在自动回弹中
 
     private final float MAX_REFRESH_LIMIT = 0.3f;//达到这个下拉临界值就开始刷新动画
+    private int mToolBarBottom;
+    private int lastIddlePos = 2;
 
     public AppBarLayoutOverScrollViewBehavior() {
     }
@@ -79,37 +84,37 @@ public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
     }
 
     @Override
-    public boolean onStartNestedScroll(CoordinatorLayout parent, AppBarLayout child, View directTargetChild, View target, int nestedScrollAxes,int type) {
-        isAnimate = true;
-        if (target instanceof DisInterceptNestedScrollView) return true;//这个布局就是middleLayout
-        return super.onStartNestedScroll(parent, child, directTargetChild, target, nestedScrollAxes,type);
-    }
-
-    @Override
     public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, int dx, int dy, int[] consumed, int type) {
-        if (!isRecovering) {
-            if (mTargetView != null && ((dy < 0 && child.getBottom() >= mParentHeight)
-                    || (dy > 0 && child.getBottom() > mParentHeight))) {
+        if (!isRecovering && child.getBottom() >= mParentHeight) {
+            if ((dy < 0 && child.getBottom() >= mParentHeight)
+                    || (dy > 0 && child.getBottom() > mParentHeight)) {//先放大--->后缩小的过程
                 scale(child, target, dy);
+
                 return;
             }
         }
-        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed,type);
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
     }
+
 
     @Override
     public boolean onNestedPreFling(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, float velocityX, float velocityY) {
-        if (velocityY > 100) {//当y速度>100,就秒弹回
+        if (velocityY > 1000)//快速滑动时，启用快速恢复静止状态
             isAnimate = false;
-        }
         return super.onNestedPreFling(coordinatorLayout, child, target, velocityX, velocityY);
     }
 
 
     @Override
     public void onStopNestedScroll(CoordinatorLayout coordinatorLayout, AppBarLayout abl, View target, int type) {
+
+        if (abl.getBottom() == mParentHeight) {//展开状态
+            lastIddlePos = 2;
+        } else if (abl.getBottom() == mToolBarBottom) {//关闭状态
+            lastIddlePos = 1;
+        }
         recovery(abl);
-        super.onStopNestedScroll(coordinatorLayout, abl, target,type);
+        super.onStopNestedScroll(coordinatorLayout, abl, target, type);
     }
 
     private void initial(AppBarLayout abl) {
@@ -117,18 +122,23 @@ public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
         mParentHeight = abl.getHeight();
         mTargetViewHeight = mTargetView.getHeight();
         mMiddleHeight = middleLayout.getHeight();
+        mToolBarBottom = mToolBar.getBottom();
     }
 
     private void scale(AppBarLayout abl, View target, int dy) {
         mTotalDy += -dy;
+        //控制target视图下拉放大上限
         mTotalDy = Math.min(mTotalDy, TARGET_HEIGHT);
+        //控制放大下限
         mLastScale = Math.max(1f, 1f + mTotalDy / TARGET_HEIGHT);
-        ViewCompat.setScaleX(mTargetView, mLastScale);
-        ViewCompat.setScaleY(mTargetView, mLastScale);
+        mTargetView.setScaleX(mLastScale);
+        mTargetView.setScaleY(mLastScale);
         mLastBottom = mParentHeight + (int) (mTargetViewHeight / 2 * (mLastScale - 1));
+        //设置AppBarLayout的bottom的位置，以实现middleLayout下面那部分内容（即layout/layout_uc_content的内容）跟随动画往下移动
         abl.setBottom(mLastBottom);
         target.setScrollY(0);
 
+        //控制middleLayout跟随动画往下移动
         middleLayout.setTop(mLastBottom - mMiddleHeight);
         middleLayout.setBottom(mLastBottom);
 
@@ -157,9 +167,8 @@ public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
 
     private void recovery(final AppBarLayout abl) {
         if (isRecovering) return;
-        if (mTotalDy > 0) {
+        if (mTotalDy > 0 && abl.getBottom() > mParentHeight) {
             isRecovering = true;
-            mTotalDy = 0;
             if (isAnimate) {
                 ValueAnimator anim = ValueAnimator.ofFloat(mLastScale, 1f).setDuration(200);
                 anim.addUpdateListener(
@@ -168,12 +177,11 @@ public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
                             public void onAnimationUpdate(ValueAnimator animation) {
 
                                 float value = (float) animation.getAnimatedValue();
-                                ViewCompat.setScaleX(mTargetView, value);
-                                ViewCompat.setScaleY(mTargetView, value);
+                                mTargetView.setScaleY(value);
+                                mTargetView.setScaleX(value);
                                 abl.setBottom((int) (mLastBottom - (mLastBottom - mParentHeight) * animation.getAnimatedFraction()));
                                 middleLayout.setTop((int) (mLastBottom -
                                         (mLastBottom - mParentHeight) * animation.getAnimatedFraction() - mMiddleHeight));
-
                                 if (onProgressChangeListener != null) {
                                     float progress = Math.min((value - 1) / MAX_REFRESH_LIMIT, 1);//计算0~1的进度
                                     onProgressChangeListener.onProgressChange(progress, true);
@@ -189,6 +197,8 @@ public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
                     @Override
                     public void onAnimationEnd(Animator animation) {
                         isRecovering = false;
+                        mTotalDy = 0;
+                        isAnimate = true;
                     }
 
                     @Override
@@ -201,17 +211,22 @@ public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
                 });
                 anim.start();
             } else {
-                ViewCompat.setScaleX(mTargetView, 1f);
-                ViewCompat.setScaleY(mTargetView, 1f);
+                mTargetView.setScaleY(1f);
+                mTargetView.setScaleX(1f);
                 abl.setBottom(mParentHeight);
                 middleLayout.setTop(mParentHeight - mMiddleHeight);
-//                middleLayout.setBottom(mParentHeight);
-                isRecovering = false;
+                middleLayout.setBottom(mParentHeight);
+
 
                 if (onProgressChangeListener != null)
                     onProgressChangeListener.onProgressChange(0, true);
+                mTotalDy = 0;
+                isRecovering = false;
+                isAnimate = true;
             }
+
         }
+
     }
 
 
